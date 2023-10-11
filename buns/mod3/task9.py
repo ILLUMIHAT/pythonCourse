d57e7944ce52a8d09db5707c48413549ea9eafc3
Mikhail Melnikov <mixas9714@mail.ru>
2023-10-12 02:11:03 +0500

    Create task9.py

---
+++ buns/mod3/task9.py
@@ -0,0 +1,29 @@
+with open('task9.txt', 'r') as file:
+    a = file.readline()
+
+a = int(a)
+x, y = 0, 0
+
+while a > 0:
+    if x == y and x >= 0:
+        while x != -y - 1 and a > 0:
+            a -= 1
+            x -= 1
+
+    if x == -y - 1:
+        while x != y and x < 0 and a > 0:
+            a -= 1
+            y -= 1
+
+    if x == y and x < 0:
+        while x != -y and a > 0:
+            a -= 1
+            x += 1
+
+    if x == -y:
+        while x != y and x > 0 and a > 0:
+            a -= 1
+            y += 1
+
+with open('task9.txt', 'a') as file:
+    file.write('\n' + str(x) + ' ' + str(y))
